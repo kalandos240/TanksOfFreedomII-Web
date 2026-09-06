8529fae15e941579058bc62855dd60a8e20568c4
Kalandos240 <124530893+kalandos240@users.noreply.github.com>
2026-09-06 12:33:18 +0300

    Handle browser focus loss for audio

--- scripts/services/audio.gd
+++ scripts/services/audio.gd
@@ -7,6 +7,7 @@ var samples = {}
 var soundtracks = {}
 var current_track = null
 var _last_requested_track = null
+var _paused_for_focus := false
 
 var master_switch = false
 var sounds_enabled = true
@@ -21,7 +22,6 @@ func _ready():
 	self.register_sample("fanfare", preload("res://assets/audio/fanfare.wav"))
 	self.register_sample("failfare", preload("res://assets/audio/failfare.wav"))
 
-
 	self.register_track("intro", preload("res://assets/audio/soundtrack/grand_beats_intro.ogg"))
 	self.register_track("menu", preload("res://assets/audio/soundtrack/grand_beats_menu_soundtrack.ogg"))
 	self.register_track("soundtrack_1", preload("res://assets/audio/soundtrack/grand_beats_soundtrack_1_metal.ogg"))
@@ -31,6 +31,18 @@ func _ready():
 	self.register_track("soundtrack_5", preload("res://assets/audio/soundtrack/reduz_the_sorrows_of_a_crab.ogg"))
 	self.register_track("soundtrack_6", preload("res://assets/audio/soundtrack/reduz_capybara_love.ogg"))
 
+func _notification(what):
+	if what == NOTIFICATION_APPLICATION_FOCUS_OUT:
+		# Browser tab/ad focus loss: keep transient SFX from continuing in background
+		# and remember whether music should resume when focus returns.
+		self._paused_for_focus = self.current_track != null and self.current_track.is_playing()
+		self.pause()
+		for sample in self.samples.values():
+			sample.stop()
+	elif what == NOTIFICATION_APPLICATION_FOCUS_IN:
+		if self._paused_for_focus and self.music_enabled:
+			self.unpause()
+		self._paused_for_focus = false
 
 func play(sample_name):
 	if not self.master_switch or not self.sounds_enabled:
@@ -85,7 +97,6 @@ func unpause(track_name=null):
 	elif self.soundtracks.has(track_name):
 		self.soundtracks[track_name].set_stream_paused(false)
 
-
 func register_sample(sample_name, stream):
 	if stream == null:
 		return
@@ -94,9 +105,7 @@ func register_sample(sample_name, stream):
 	self.get_tree().get_root().call_deferred("add_child", sfx)
 	sfx.set_stream(stream)
 	sfx.set_bus(self.BUS_SFX)
-
 	self.samples[sample_name] = sfx
-
 
 func register_track(track_name, stream):
 	if stream == null:
@@ -106,7 +115,6 @@ func register_track(track_name, stream):
 	self.get_tree().get_root().call_deferred("add_child", new_track)
 	new_track.set_stream(stream)
 	new_track.set_bus(self.BUS_MUSIC)
-
 	self.soundtracks[track_name] = new_track
 
 func restart_track():
